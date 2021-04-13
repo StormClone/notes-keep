09a170547010c020a2a717ad43a08d350617db73
StormClone <michael_5095@hotmail.com>
2021-04-13 00:06:31 -0700

    Add icon

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
+const favicon = require('express-favicon');
 const mongoose = require('mongoose');
 
 const app = express();
@@ -10,6 +11,7 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use(favicon(__dirname + '../notes-app/public/icon-notes.ico'));
 
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true} );
